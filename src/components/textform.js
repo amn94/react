0ef0a97b8ac5fbb4342ce4dd5f527b7d231dac94
amn94 <112054577+amn94@users.noreply.github.com>
2023-06-13 13:07:15 +0530

    Add files via upload

--- src/components/textform.js
+++ src/components/textform.js
@@ -27,6 +27,23 @@ export default function Textform(props) {
   
    
   }
+  const handleClClick=()=>{
+    console.log("clear  was clicked" + text)
+    // let newtext=text.toLowerCase();
+    setText(' ');
+  
+   
+  }
+   function handleCaClick(){
+    console.log("capitalized was clicked" + text)
+    // let newtext=text.toLowerCase();
+    // setText(newtext);
+     const capitalizedText = text.replace(/\b\w/g, (match) => match.toUpperCase());
+    setText(capitalizedText);
+   
+  
+   
+  }
   const handOnChange=(event)=>{
     console.log("change was there")
     // setText("you have click on handochange")
@@ -34,26 +51,42 @@ export default function Textform(props) {
   }
   const [text,setText]= useState('enter text here');
   //  setText("new text");
+
+  const handleCopy=()=>{
+    console.log("clear  was clicked" )
+     var text = document.getElementById("mybox");
+    text.select();
+    // let newtext=text.toLowerCase();
+    // setText(newtext);
+    navigator.clipboard.writeText(text.value);
+  
+   
+  }
+  
   return (
     
     <div className= "box">
         <h1>{props.title}</h1>
         <div>
-        {/* <label for ="mybox" class="form"><h1>Example Textarea</h1> </label> */}
+        {/* <label for ="mybox" className="form"><h1>Example Textarea</h1> </label> */}
         <textarea  style={{ width: '1300px',margin :'50px' }} value={text} onChange={handOnChange} className='box2' id='mybox'  rows='7'></textarea>
         </div>
-        <button className="but" style= {{margin:'20px'}} onClick={handleUpClick}>convert to upper case</button>
-        <button className="but" style= {{margin:'20px'}} mx-2 onClick={handleLoClick}>convert to lower case</button>
-        
-      <h1>Preview</h1>
+        <button className="but" style= {{margin:'20px'}}  onClick={handleUpClick}>convert to upper case</button>
+        <button className="but" style= {{margin:'20px'}}  onClick={handleLoClick}>convert to lower case</button>
+        <button className="but" style= {{margin:'20px'}}  onClick={handleCaClick}>capitilized case</button>
+        <button className="but" style= {{margin:'20px'}}  onClick={handleClClick}>clear</button>
+        <button className="but" style= {{margin:'20px'}}  onClick={handleCopy}>copy</button>
+         
+         <h1>Preview</h1>
       <p>{text}</p>
-      <div className='container'>
+         <div className='container'>
           <p>{text.split(" ").length} words and {text.length} characters </p>
           <p>{0.008 *text.split(" ").length} minutes to read</p>
           <p>{0.028 *text.split(" ").length} minutes to speak</p>
-      </div>
+         </div>
+                       
+         </div> 
 
-     </div>  
       
 
   )
