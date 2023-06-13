ab3ec57684de66a8ede9f225e5772fcde09df078
amn94 <112054577+amn94@users.noreply.github.com>
2023-06-13 12:53:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/navbar';
-// let name ="Aman"
-function App() {
-  return (
-      <>
-       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-         <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    </div>
-         <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    </div>
-         <div className="container-fluid">
-    <a className="navbar-brand" href="/">Home</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    </div>
-         <div className="container-fluid">
-    <a className="navbar-brand" href="/">About</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+import Navbar from "./components/navbar";
+import Textform from './components/textform';
+import About from './components/about';
+import React, { useState} from 'react';
 
-    </div>
-    
-</nav>
-<Navbar></Navbar>
+// let name ="Aman"
+
+function App() {
+  const [mode,setMode]=useState('dark');
+  return (
+<>
+<Navbar title ="home" about ="about us" mode ={mode}/> 
+{/* <div className="container"></div> */}
+ <Textform  title ="Enter Text Here"  />
+ <About />
    </>
   );
 }
